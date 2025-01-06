567087767fd77648a14a7c4b482d9a752907bc13
james-eo <jameseokonkwo@gmail.com>
2025-01-06 08:49:14 +0100

    Added register and login routes

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '695a4c8ec0498e2dfd972fa8cf8f31a00a0a0e'
 
 posts = [
     {
@@ -26,6 +29,20 @@ def home():
 @app.route('/about')
 def about():
     return render_template("about.html", title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect("home")
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 
 if __name__=='__main__':
