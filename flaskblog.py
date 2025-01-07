da03c93237abe511e2d60a5e638ec43abcbb506f
james-eo <jameseokonkwo@gmail.com>
2025-01-07 06:55:09 +0100

    Added form validation logic

--- flaskblog.py
+++ flaskblog.py
@@ -38,9 +38,15 @@ def register():
         return redirect("home")
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'info@flaskblog.com' and form.password.data == 'password':
+            flash('You have been successfully logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash("Invalid credentials. Check your email and password", "danger")
     return render_template('login.html', title='Login', form=form)
 
 
