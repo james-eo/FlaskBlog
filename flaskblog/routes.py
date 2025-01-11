7ed00b11cc5eeee94597cfab2a0315c83cd4be2a
james-eo <jameseokonkwo@gmail.com>
2025-01-11 14:02:52 +0100

    Created logout route

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user
 
 posts = [
     {
@@ -52,8 +52,13 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            flash(f"Login successful for {form.email.data}!", "success")
+            flash(f"Logged in successfully", "success")
             return redirect(url_for("home"))
         else:
             flash("Invalid credentials. Please check your email and password", "danger")
     return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
