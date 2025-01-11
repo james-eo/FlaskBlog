7f370da61e8fa783eccfbdd7912baddf28123b95
james-eo <jameseokonkwo@gmail.com>
2025-01-11 10:18:08 +0100

    Used timezone-aware datetime objects by replacing the deprecated datetime.utcnow() with datetime.now(timezone.utc) to ensure better datetime comparisons and conversions

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from flaskblog import db
 
 
@@ -17,7 +17,7 @@ class User(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
