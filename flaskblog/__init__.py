99f2e948126b24c20603c5fc89e157cac9a08e59
james-eo <jameseokonkwo@gmail.com>
2025-01-11 10:45:54 +0100

    Imported and initialised flask LoginManager

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app.config['SECRET_KEY'] = '695a4c8ec0498e2dfd972fa8cf8f31a00a0a0e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskblog.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 
 app.app_context().push()
