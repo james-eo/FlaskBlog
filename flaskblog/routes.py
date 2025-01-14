7344a2c316d7add4431b45f1a77d210d5eea7637
james-eo <jameseokonkwo@gmail.com>
2025-01-14 15:13:40 +0100

    Resized profile images with PIL.Image.thumbnail method of Pillow liberary

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -80,7 +81,12 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+
+    image_size = (125, 125)
+    image = Image.open(form_picture)
+    image.thumbnail(image_size)
+
+    image.save(picture_path)
     return picture_fn
 
 @app.route("/account", methods=['GET', 'POST'])
