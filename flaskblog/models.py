82bb35381056ef6b34b2df9be30b736872f7631b
james-eo <jameseokonkwo@gmail.com>
2025-01-17 13:36:40 +0100

    Created password reset token methods using itsdangerous

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
-from flaskblog import db, login_manager
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 
@@ -14,6 +15,20 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(120), nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        s.dumps({'user_id': self.id})
+        return s
+    
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,10 +2,11 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort 
-from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flaskblog import app, db, bcrypt, mail
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
 
 
 @app.route("/")
@@ -149,3 +150,45 @@ def user_posts(username):
     posts = Post.query.filter_by(author=user)\
         .order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('user_posts.html', posts=posts, user=user)
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message("Password Reset Request", sender="noreply@ejiks.com", recipients=[user.email])
+    msg.body = f"""To reset your password, visit the following link:
+{url_for('reset_password_token', token=token, _external=True)}
+If you did not make this request, please ignore this email and no changes will be made.
+"""
+    mail.send(msg)
+    # flash(f"An email has been sent with instructions to reset your password", "info")
+    # return redirect(url_for("login"))
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_reset_email(user)
+            return redirect(url_for("login"))
+        else:
+            flash("Email not found. Please check your email and try again", "danger")
+    return render_template("reset_request.html", title="Reset Password", form=form)
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_password_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash("Invalid or expired token. Please try again", "danger")
+        return redirect(url_for("reset_password"))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash("Your password has been updated successfully!", "success")
+        return redirect(url_for("login"))
+    return render_template("reset_token.html", title="Reset Password", form=form)
