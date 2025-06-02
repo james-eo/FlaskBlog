f4b4d4af62d56bf289021127b2e22c94fb0646b3
james-eo <jamesejike04@gmail.com>
2025-06-02 23:04:41 +0100

    Added doctsring documentation

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -4,6 +4,13 @@ from wtforms.validators import DataRequired
 
 
 class PostForm(FlaskForm):
+    """Form class for creating or updating a blog post.
+
+    Attributes:
+        title (StringField): Field for the post title. Required.
+        content (TextAreaField): Field for the post content. Required.
+        submit (SubmitField): Submit button labeled 'Post'.
+    """
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
